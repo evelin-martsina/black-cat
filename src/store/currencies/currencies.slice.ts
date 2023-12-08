d212c6d9e7972b6c3793a6ef60515dbde6844f83
Evelin Martsina <e.martsina@astondevs.ru>
2023-12-08 11:29:09 +0300

    fix: format

--- src/store/currencies/currencies.slice.ts
+++ src/store/currencies/currencies.slice.ts
@@ -1,15 +1,15 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-import { CurrenciesData } from 'src/types';
+import { ICurrenciesData } from 'src/types';
 import { baseUrl } from './constants';
 
 export const currenciesApi = createApi({
     reducerPath: 'currencies',
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
-        getCurrencies: builder.query<CurrenciesData[], void>({
+        getCurrencies: builder.query<ICurrenciesData[], void>({
             query: () => 'currencies',
-            transformResponse: (res: { data: CurrenciesData[] }) => res.data,
+            transformResponse: (res: { data: ICurrenciesData[] }) => res.data,
         }),
     }),
 });
